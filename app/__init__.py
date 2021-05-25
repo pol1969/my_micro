67e0f729d41943388cea7586977b0036d39e781d
pol1969 <podberezsky1969@gmail.com>
2021-05-25 21:33:16 +0300

    Готовим к Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
 from elasticsearch import Elasticsearch
 from config import Config
-
+ 
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
@@ -21,12 +21,12 @@ mail = Mail()
 bootstrap = Bootstrap()
 moment = Moment()
 babel = Babel()
-
-
+ 
+ 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-
+ 
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
@@ -36,16 +36,16 @@ def create_app(config_class=Config):
     babel.init_app(app)
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
-
+ 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
-
+ 
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
-
+ 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
-
+ 
     if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
             auth = None
@@ -62,26 +62,32 @@ def create_app(config_class=Config):
                 credentials=auth, secure=secure)
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
-
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/microblog.log',
-                                           maxBytes=10240, backupCount=10)
-        file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s: %(message)s '
-            '[in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
-
+ 
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/microblog.log',
+                                               maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+ 
         app.logger.setLevel(logging.INFO)
         app.logger.info('Microblog startup')
-
+ 
     return app
-
-
+ 
+ 
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(current_app.config['LANGUAGES'])
-
-
+ 
+ 
 from app import models
+ 
